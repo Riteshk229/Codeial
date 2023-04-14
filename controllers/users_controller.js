f97e6ae365d1e360d8cd288946b794b17fbec021
Ritesh <Riteshk229@gmail.com>
2023-04-14 20:07:35 +0530

    set up assets and layouts

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,15 @@
 module.exports.profile = function(req,res){
-    return res.end('<h1> User Profile </h1>');
+    // return res.end('<h1> User Profile </h1>');
+
+    return res.render('users_profile',{
+        title: "Profile"
+    });
 };
 
 module.exports.post = function(req,res){
-    return res.end('<h1> User Posts </h1>')
+    // return res.end('<h1> User Posts </h1>')
+
+    return res.render('users_post',{
+        title: "Post"
+    });
 };
