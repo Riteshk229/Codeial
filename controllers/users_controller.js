ab9d6d698da350a22083bc8c06fee45375ec92f4
Ritesh <Riteshk229@gmail.com>
2023-04-17 20:34:23 +0530

    created sign-out and authorised the sign-in part using cookies

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,15 +2,32 @@ const User = require('../models/user')
 
 module.exports.profile = function(req,res){
     // return res.end('<h1> User Profile </h1>');
-
-    return res.render('users_profile',{
-        title: "Profile"
-    });
+    
+    // after sign-in
+    if(req.cookies.user_id){
+        // find user
+        User.findById(req.cookies.user_id).then(function(user,err){
+            if(user){
+                return res.render('users_profile',{
+                    title: "Profile",
+                    user : user
+                });
+            }
+            return res.redirect('/users/sign-in');
+        });
+    }
+    else{
+        return res.redirect('/users/sign-in');
+    }
+    
+    // return res.render('users_profile',{
+    //     title: "Profile"
+    // });
 };
 
 module.exports.post = function(req,res){
     // return res.end('<h1> User Posts </h1>')
-
+    
     return res.render('users_post',{
         title: "Post"
     });
@@ -33,18 +50,18 @@ module.exports.signIn = function(req,res){
 
 // get signup data
 module.exports.create = function(req,res){
-    console.log(`${req.body} in create`);
+    // console.log(`${req.body} in create`);
     if(req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
-
+    
     User.findOne({email : req.body.email}).then(function(user,err){
         if(err){
             console.log("Error in finding user in signing up");
             return;
         }
-
-
+        
+        
         if(!user){
             User.create(req.body).then(function(user,err){
                 if(err){
@@ -58,38 +75,47 @@ module.exports.create = function(req,res){
             return res.redirect('back');
         }
     });
-
+    
 };
 
 // Signin and create session for user
 module.exports.createSession = function(req,res){
-    console.log(`${req.body} in create session`);
-
+    // console.log(`${req.body.name} in create session`);
+    
     // find the user
     User.findOne({email: req.body.email}).then(function(user,err){
         if(err){
             console.log("Error in creating user while signing up");
             return ;
         }
-
+        
         // user found
         if(user){
-
+            
             // password doesn't match
             if(user.password != req.body.password){
                 return res.redirect('back');
             }
-
+            
             // session creation
             res.cookie('user_id',user.id);
             return res.redirect('/users/profile');
-
+            
         }
         // user not found
         else{
             return res.redirect('/users/sign-up');
         }
-
-
+        
+        
     });
 }
+
+module.exports.signOut = function(req,res){
+    console.log(req.cookies);
+    res.cookie("user_id"," ",{
+        maxAge: 0
+    });
+
+    return res.redirect('/users/sign-in');
+};
