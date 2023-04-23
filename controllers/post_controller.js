c486047a7746723a93f78d6c012f4964f892d113
Ritesh <Riteshk229@gmail.com>
2023-04-23 18:56:15 +0530

    adjusted some view and scss

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comments');
 
 module.exports.create =  function(req,res){
     Post.create ({
@@ -13,4 +14,28 @@ module.exports.create =  function(req,res){
         return res.redirect('back');
     });
 
+};
+
+module.exports.destroy = function(req,res){
+
+    Post.findById(req.params.id).then(function(post,err){
+
+        // .id is used to convert objects into strings
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post : req.params.id}).then(function(comment,err){
+                if(err){
+                    console.log('Error in deleting the comment')
+                    return;
+                }
+
+                return res.redirect('back');
+            });
+        }
+        else{
+                
+            return res.redirect('back');
+        }
+    });
 };
